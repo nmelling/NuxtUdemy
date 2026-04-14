f024ec5422d6fd6b07ea54573e61ae8fcbc18ada
nmellinger <nicolas.mellinger@terascop.com>
2026-04-14 08:14:40 +0200

    definePageMeta concept

--- app/pages/users/index.vue
+++ app/pages/users/index.vue
@@ -1,3 +1,20 @@
 <template>
     <h1>Users</h1>
 </template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+// Ne définit pas les headers html d'une page mais permet d'ajouter des propriétés à la route
+// Ne peut pas utiliser de propriétés réactives
+  definePageMeta({
+    title: 'Users',
+    meta: [
+      { name: 'description', content: 'Users description...' },
+    ],
+    something: 'Hey...'
+  })
+
+// Contient les infos définie au sein de definePageMeta
+console.log(route.meta);
+</script>
