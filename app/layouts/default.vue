c908eca591e0515bc5aa4afca580b26b95a6a853
nmellinger <nicolas.mellinger@terascop.com>
2026-05-03 20:32:36 +0200

    routing -- link

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -7,6 +7,20 @@
   <div class="container">
       <header>
           <h1>Header</h1>
+          <!-- LINKS -->
+          <nav>
+              <ul>
+                  <li>
+                      <NuxtLink to="/">Home</NuxtLink>
+                  </li>
+                  <li>
+                      <NuxtLink
+                        to="/contact"
+                        target="_blank"
+                    >Contact</NuxtLink>
+                  </li>
+              </ul>
+          </nav>
       </header>
       <hr />
       <slot />
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,13 @@
 <template>
     <h1>Home</h1>
+    <hr>
+    <button @click="router.push('/contact')">
+        Go to contacts
+    </button>
 </template>
+
+<script setup lang="ts">
+const router = useRouter();
+
+
+</script>
